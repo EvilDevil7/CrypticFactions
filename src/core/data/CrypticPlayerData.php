3b9bfa56b7f3c50ec5db0c3144d6082b05b22823
MakerbakerYT <65129039+MakerbakerYT@users.noreply.github.com>
2020-05-10 22:02:01 -0600

    Create CrypticPlayerData.php

---
+++ src/core/data/CrypticPlayerData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace core\data;
+
+use core\crate\CratePlayerData;
+
+class CrypticPlayerData {
+
+    /** @var CratePlayerData */
+    private $crateData;
+
+    public function __construct()
+    {
+    }
+
+}
